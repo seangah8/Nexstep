de9f6dddc824d2ae122a20ae9e5edf0f0b0e1620
seangah8 <seangah8@gmail.com>
2025-11-18 10:49:33 +0200

    chore: update http service to connect into backend based on .env

--- frontend/src/services/http.service.ts
+++ frontend/src/services/http.service.ts
@@ -1,6 +1,6 @@
 import Axios, { AxiosRequestConfig, Method } from 'axios'
 
-const BASE_URL = 'http://localhost:3000/api/'
+const BASE_URL = import.meta.env.BASE_URL
 
 const axios = Axios.create({ withCredentials: true })
 
