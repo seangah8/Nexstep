2ef91969eda6909ce627e064d41508375c40fb08
seangah8 <seangah8@gmail.com>
2025-11-22 05:56:42 +0200

    chore: update new domain name in cors

--- backend/server.ts
+++ backend/server.ts
@@ -23,7 +23,7 @@ const corsOptions = {
   origin: [
     'http://127.0.0.1:5173', 
     'http://localhost:5173',
-    'https://nexstepp.vercel.app',
+    'https://nextepp.vercel.app',
   ],
   credentials: true,
 }
